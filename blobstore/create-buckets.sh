40bc72a0d726cef04422087d3b7bdd683b63470d
jarno <wiemanboy@gmail.com>
2024-07-13 22:24:44 +0200

    UPDATE: made buckets private and now only use wiemancdn

--- blobstore/create-buckets.sh
+++ blobstore/create-buckets.sh
@@ -17,10 +17,8 @@ mc alias set minio http://127.0.0.1:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWOR
 
 # Create buckets
 mc mb minio/wiemansite || true
-mc anonymous set download minio/wiemansite
 
 mc mb minio/wiemannl || true
-mc anonymous set download minio/wiemansite
 
 # Stop Minio
 kill -s INT $(cat /tmp/minio.pid) && rm /tmp/minio.pid
